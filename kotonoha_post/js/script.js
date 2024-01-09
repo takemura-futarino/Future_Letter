0c54705161bfc8038f955ca778e22c5ee57d6736
takemura sei <takemura.futarino@gmail.com>
2024-01-09 15:55:36 +0900

    タブ機能完成

--- kotonoha_post/js/script.js
+++ kotonoha_post/js/script.js
@@ -7,7 +7,6 @@ if (!allowedHostnames.includes(window.location.hostname)) {
     // 必要に応じて他のメソッドも無効化
 }
 
-
 // LIFFの初期化を行う
 liff
 .init({
@@ -24,20 +23,20 @@ liffId: "2000014015-QqLAlNmW"
 
         //callApi()関数の呼び出し
         await callApi(accessToken);
-        // letter_indexApi()関数の呼び出し
+        //letter_indexApi()関数の呼び出し
         const postdata = await letter_indexApi(accessToken);
         console.log(postdata);
-        // letter_data()関数の呼び出し
-        await letter_data(postdata);
-        // change_data()関数の呼び出し
-        await change_data(postdata);
+        //partnerLetter()関数の呼び出し
+        await partnerLetter(postdata);
+        //tabMenu()関数の呼び出し
+        await tabMemu(postdata);
+
     }
 })
 .catch((err) => {
     //初期化中にエラーが発生します
     console.error(err.code, err.message);
 });
-
 
 //--------アクセストークンを登録する--------
 async function callApi(accessToken) {
@@ -77,95 +76,171 @@ async function letter_indexApi(accessToken) {
     }
 } 
 
-//---------手紙を表示させる----------
-async function letter_data(postdata) {
+//-------- 起動時の挙動＆パートナーからの手紙一覧 ---------
+async function partnerLetter(postdata) {
     try {
-        if (postdata.data.from_partner.length !== 0) {
-            const non_item = document.querySelector('.non_item');
-            console.log(non_item);
-            non_item.classList.add('comment_flash');
-        }
+        let letterList = document.querySelector(".letter_list");
 
-        const post_number = postdata.data.from_partner;
-        const partnerList = document.querySelector('.partner_list');
+        if (postdata.data.from_partner === null) {
+            letterList.innerHTML += "<div class='non_item'><p>このフォルダーは空です</p><span>想いを手紙に載せて届けましょう。</span></div>";
+        } else {
+            const post_number = postdata.data.from_partner;
+            const partnerList = document.querySelector('.partner_list');
 
-        for (let i = 0; i < post_number.length; i++) {
-            // 新しい <li> 要素を作成
-            const newListItem = document.createElement("li");
-            // 新しい <a> 要素を作成
-            const newListURL = document.createElement("a"); 
-            // <a> にクラスを追加   
-            newListURL.classList.add("newID"); 
-            // 未開封か既読かの確認
-            if (post_number[i].is_read === 0) {
-                // 画像の情報
-                const imageUrl = "image/unopened_male.png";
-                const imageAltText = "未開封の手紙";
-                // <img> 要素を作成
-                const imageElement = document.createElement("img");
-                imageElement.src = imageUrl;
-                imageElement.alt = imageAltText;
-                // <img> にクラスを追加
-                imageElement.classList.add("letter_number");
-                // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
-                imageElement.addEventListener('click', () => {
-                    const value = post_number[i].id;
-                    window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view?value=${value}`;
-                });
-                // <a> に <img> を追加
-                newListURL.appendChild(imageElement);
-            
-            } else if (post_number[i].is_read === 1) {
-            // 既読の場合の処理
-                // 画像の情報
-                const imageUrl = "image/opened_male.png";
-                const imageAltText = "開封済みの手紙";
-                // <img> 要素を作成
-                const imageElement = document.createElement("img");
-                imageElement.src = imageUrl;
-                imageElement.alt = imageAltText;
-                // <img> にクラスを追加
-                imageElement.classList.add("letter_number");
-                // どの手紙を押したのかを手紙閲覧ページに伝える必要がある。 id を送る。
-                // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
-                imageElement.addEventListener('click', () => {
-                    const value = post_number[i].id;
-                    window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view?value=${value}`;
-                });
-                // <a> に <img> を追加
-                newListURL.appendChild(imageElement);
+            for (let i = 0; i < post_number.length; i++) {
+                // 新しい <li> 要素を作成
+                const newListItem = document.createElement("li");
+                // 新しい <a> 要素を作成
+                const newListURL = document.createElement("a"); 
+                // <a> にクラスを追加   
+                newListURL.classList.add("newID"); 
+                // 未開封か既読かの確認
+                if (post_number[i].is_read === 0) {
+                    // 画像の情報
+                    const imageUrl = "image/unopened_male.png";
+                    const imageAltText = "未開封の手紙";
+                    // <img> 要素を作成
+                    const imageElement = document.createElement("img");
+                    imageElement.src = imageUrl;
+                    imageElement.alt = imageAltText;
+                    // <img> にクラスを追加
+                    imageElement.classList.add("letter_number");
+                    // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
+                    imageElement.addEventListener('click', () => {
+                        const value = post_number[i].id;
+                        window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view?value=${value}`;
+                    });
+                    // <a> に <img> を追加
+                    newListURL.appendChild(imageElement);
+                
+                } else if (post_number[i].is_read === 1) {
+                // 既読の場合の処理
+                    // 画像の情報
+                    const imageUrl = "image/opened_male.png";
+                    const imageAltText = "開封済みの手紙";
+                    // <img> 要素を作成
+                    const imageElement = document.createElement("img");
+                    imageElement.src = imageUrl;
+                    imageElement.alt = imageAltText;
+                    // <img> にクラスを追加
+                    imageElement.classList.add("letter_number");
+                    // どの手紙を押したのかを手紙閲覧ページに伝える必要がある。 id を送る。
+                    // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
+                    imageElement.addEventListener('click', () => {
+                        const value = post_number[i].id;
+                        window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view?value=${value}`;
+                    });
+                    // <a> に <img> を追加
+                    newListURL.appendChild(imageElement);
+                }
+                // 新しい要素 <p> 要素を作成
+                const newListDay = document.createElement("p");
+                // <p> に日付の情報を入れる
+                newListDay.textContent = post_number[i].send_at;
+                // <a> に <p> を追加
+                newListURL.appendChild(newListDay);
+                // <li> に <a> を追加
+                newListItem.appendChild(newListURL);
+                // <ul> に <li> を追加
+                partnerList.appendChild(newListItem);
             }
-            // 新しい要素 <p> 要素を作成
-            const newListDay = document.createElement("p");
-            // <p> に日付の情報を入れる
-            newListDay.textContent = post_number[i].send_at;
-            // <a> に <p> を追加
-            newListURL.appendChild(newListDay);
-            // <li> に <a> を追加
-            newListItem.appendChild(newListURL);
-            // <ul> に <li> を追加
-            partnerList.appendChild(newListItem);
         }
-
     } catch (error) {
         console.error(error);
     }
 }
 
-// -----------手紙を切り替える-----------
-async function change_data(postdata) {
+//--------- 自分自身の手紙一覧 ----------
+async function myselfLetter(postdata) {
+    try {
+        if (postdata.data.from_me === null) {
+            letterList.innerHTML += "<div class='non_item'><p>このフォルダーは空です</p><span>想いを手紙に載せて届けましょう。</span></div>";
+        } else {
+            const post_number = postdata.data.from_me;
+            const myselfList = document.querySelector('.myself_list');
+    
+            for (let i = 0; i < post_number.length; i++) {
+                // 新しい <li> 要素を作成
+                const newListItem = document.createElement("li");
+                // 新しい <a> 要素を作成
+                const newListURL = document.createElement("a"); 
+                // <a> にクラスを追加   
+                newListURL.classList.add("newID"); 
+                // 未開封か既読かの確認
+                if (post_number[i].is_read === 0) {
+                    // 画像の情報
+                    const imageUrl = "image/unopened_male.png";
+                    const imageAltText = "未開封の手紙";
+                    // <img> 要素を作成
+                    const imageElement = document.createElement("img");
+                    imageElement.src = imageUrl;
+                    imageElement.alt = imageAltText;
+                    // <img> にクラスを追加
+                    imageElement.classList.add("letter_number");
+                    // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
+                    imageElement.addEventListener('click', () => {
+                        const value = post_number[i].id;
+                        window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view?value=${value}`;
+                    });
+                    // <a> に <img> を追加
+                    newListURL.appendChild(imageElement);
+                
+                } else if (post_number[i].is_read === 1) {
+                // 既読の場合の処理
+                    // 画像の情報
+                    const imageUrl = "image/opened_male.png";
+                    const imageAltText = "開封済みの手紙";
+                    // <img> 要素を作成
+                    const imageElement = document.createElement("img");
+                    imageElement.src = imageUrl;
+                    imageElement.alt = imageAltText;
+                    // <img> にクラスを追加
+                    imageElement.classList.add("letter_number");
+                    // どの手紙を押したのかを手紙閲覧ページに伝える必要がある。 id を送る。
+                    // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
+                    imageElement.addEventListener('click', () => {
+                        const value = post_number[i].id;
+                        window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view?value=${value}`;
+                    });
+                    // <a> に <img> を追加
+                    newListURL.appendChild(imageElement);
+                }
+                // 新しい要素 <p> 要素を作成
+                const newListDay = document.createElement("p");
+                // <p> に日付の情報を入れる
+                newListDay.textContent = post_number[i].send_at;
+                // <a> に <p> を追加
+                newListURL.appendChild(newListDay);
+                // <li> に <a> を追加
+                newListItem.appendChild(newListURL);
+                // <ul> に <li> を追加
+                myselfList.appendChild(newListItem);
+            } 
+        }
+  
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//--------- タブメニュー ----------
+async function tabMemu(postdata) {
     try {
         const partnerTab = document.querySelector(".partner_tab");
-        const oneselfTab = document.querySelector(".oneself_tab");
-
+        let letterList = document.querySelector(".letter_list");
         partnerTab.addEventListener('click', () => {
-            letter_data(postdata);
+            // 重複を避けるために<ul>内を一度空にする必要がある
+            letterList.innerHTML = "";
+            partnerLetter(postdata);
         });
 
-        oneselfTab.addEventListener('click', () => {
-            console.log("切り替えボタン");
+        const oneself_tab = document.querySelector(".oneself_tab");
+        oneself_tab.addEventListener('click', () => {
+            letterList.innerHTML = "";
+            myselfLetter(postdata);
         })
     } catch (error) {
         console.error(error);
     }
 }
+
