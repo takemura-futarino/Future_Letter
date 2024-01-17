9704feb0e2b4286ab950ca868e21d54d38b720eb
takemura sei <takemura.futarino@gmail.com>
2024-01-17 17:24:41 +0900

    お預かりBOX完成

--- custody_post/js/script.js
+++ custody_post/js/script.js
@@ -25,7 +25,10 @@ liffId: "2000014015-QqLAlNmW"
         //callApi()関数の呼び出し
         await callApi(accessToken);
         //letter_indexApi()関数の呼び出し
-        await letter_indexApi(accessToken);
+        const postdata = await letter_indexApi(accessToken);
+        console.log(postdata);
+        //partnerLetter()関数の呼び出し
+        await partnerLetter(postdata);
 
     }
 })
@@ -57,6 +60,7 @@ async function callApi(accessToken) {
     }
 }
 
+
 //--------手紙一覧の取得----------
 async function letter_indexApi(accessToken) {
     try {
@@ -66,56 +70,93 @@ async function letter_indexApi(accessToken) {
             }
         });
         const postdata = await getLetter.json();
-        console.log(postdata);
-        const post_number = postdata.data.is_sending;
-        console.log(post_number); //届いている手紙の数の確認
-
-        //　パートナーからの手紙がある場合の挙動
-        const non_item = document.querySelector('.non_item');
-        if (post_number.length !== 0) {
-            non_item.classList.add('comment_flash');
-            await letter_data(post_number);
-        }
+        return postdata;
 
     } catch (error) {
         console.error(error);
     }
 } 
 
-//----------手紙一覧の表示---------
-async function letter_data(post_number) {
-    const letter_list = document.querySelector('.letter_list');
 
-    for (let i = 0; i < post_number.length; i++) {
-        // 新しい <li> 要素を作成
-        const newListItem = document.createElement("li");
-        // 画像の情報
-        const imageUrl = "image/unopened_male.png";
-        const imageAltText = "お預かり中の手紙";
-        // <img> 要素を作成
-        const imageElement = document.createElement("img");
-        imageElement.src = imageUrl;
-        imageElement.alt = imageAltText;
-        // <img> にクラスを追加
-        imageElement.classList.add("letter_number");
-        // 宛先がパートナーなら
-        if (post_number[i].send_to === 2) {
-            // どの手紙を押したのかを手紙閲覧ページに伝える必要がある。 id を送る。
-            // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
-            imageElement.addEventListener('click', () => {
-                const value = post_number[i].id;
-                window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view_custody?value=${value}`;
-            })
+//-------- 起動時の挙動＆パートナーからの手紙一覧 ---------
+async function partnerLetter(postdata) {
+    try {
+        let letterList = document.querySelector(".letter_list");
+
+        if (postdata.data.from_partner === null) {
+            letterList.innerHTML += "<div class='non_item'><p>このフォルダーは空です</p><span>想いを手紙に載せて届けましょう。</span></div>";
+        } else {
+            const post_number = postdata.data.is_sending;
+            console.log(post_number);
+
+            for (let i = 0; i < post_number.length; i++) {
+                // 新しい <li> 要素を作成
+                const newListItem = document.createElement("li");
+                // 新しい <div> 要素を作成
+                const newListURL = document.createElement("div"); 
+                // <div> にクラスを追加   
+                newListURL.classList.add("newID"); 
+                // パートナー宛か自分宛かの確認
+                // パートナー宛の場合
+                if (post_number[i].send_to === 2) {
+                    // 画像の情報
+                    const imageUrl = "image/unopened_male.png";
+                    const imageAltText = "パートナー宛てのコトノハ";
+                    // <img> 要素を作成
+                    const imageElement = document.createElement("img");
+                    imageElement.src = imageUrl;
+                    imageElement.alt = imageAltText;
+                    // <img> にクラスを追加
+                    imageElement.classList.add("letter_number");
+                    // <img> にクリックイベント追加/idを割り振った手紙閲覧ページのURL発行
+                    imageElement.addEventListener('click', () => {
+                        const value = post_number[i].id;
+                        window.location.href = `https://liff.line.me/2000014015-QqLAlNmW/view_custody?value=${value}`;
+                    });
+                    // <div> に <img> を追加
+                    newListURL.appendChild(imageElement);
+                
+                // 自分宛の場合
+                } else if (post_number[i].send_to === 1) {
+                    // 画像の情報
+                    const imageUrl = "image/unopened_male.png";
+                    const imageAltText = "自分宛てのコトノハ";
+                    // <img> 要素を作成
+                    const imageElement = document.createElement("img");
+                    imageElement.src = imageUrl;
+                    imageElement.alt = imageAltText;
+                    // <img> にクラスを追加
+                    imageElement.classList.add("letter_number");
+                    // <div> に <img> を追加
+                    newListURL.appendChild(imageElement);
+                }
+                // 新しい要素 <p> 要素を作成
+                const newListDay = document.createElement("p");
+
+                // 変換した日付情報
+                const newDateinfo = post_number[i].send_at
+                // Dateオブジェクトを作成して日付文字列を解析
+                var dateObject = new Date(newDateinfo);
+                // 年、月、日を取得
+                var year = dateObject.getFullYear();
+                var month = dateObject.getMonth() + 1; // 月は0から11で表されるため、1を加える
+                var day = dateObject.getDate();
+                // フォーマットした日付文字列を作成
+                var formattedDateString = year + "年" + month + "月" + day + "日";
+                // 結果をコンソールに表示
+                console.log(formattedDateString);
+
+                // <p> に日付の情報を入れる
+                newListDay.textContent = formattedDateString;
+                // <div> に <p> を追加
+                newListURL.appendChild(newListDay);
+                // <li> に <a> を追加
+                newListItem.appendChild(newListURL);
+                // <ul> に <li> を追加
+                letterList.appendChild(newListItem);
+            }
         }
-        // <li> に <img> を追加
-        newListItem.appendChild(imageElement);
-        // 新しい要素 <p> 要素を作成
-        const newListDay = document.createElement("p");
-        // <p> に日付の情報を入れる
-        newListDay.textContent = post_number[i].send_at;
-        // <li> に <p> を追加
-        newListItem.appendChild(newListDay);
-        // <ul> に <li> を追加
-        letter_list.appendChild(newListItem);
+    } catch (error) {
+        console.error(error);
     }
 }
